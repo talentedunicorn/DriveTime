21c0093342f344dc26d97d781c2bca6b9ad7e521
Ezekiel Mangalu <emangalu@gmail.com>
2015-06-11 12:11:18 +0800

    Added distance matrix script and basic query function

--- js/app.js
+++ js/app.js
@@ -1,2 +1,35 @@
-// Set some defaults
-var googleDMAPIKey = 'AIzaSyAIsXp5W401xXH3q4JTiClS3JwvFW6wNtQ';
+// Load Distance Matrix Service
+var distanceMatrixService;
+
+GoogleMapsLoader.load(function (google) {
+	distanceMatrixService = new google.maps.DistanceMatrixService();
+});
+
+// Query
+function getDistance(origin, destination, travelMode) {
+	distanceMatrixService.getDistanceMatrix({
+		origins: [origin],
+		destinations: [destination],
+		travelMode: travelMode || google.maps.TravelMode.DRIVING
+	}, callback);
+}
+
+function callback(response, status) {
+	if (status == google.maps.DistanceMatrixStatus.OK) {
+		var origins = response.originAddresses,
+			destinations = response.destinationAddresses;
+
+		for (var i = 0; i < origins.length; i++) {
+			var results = response.rows[i].elements;
+			for (var j = 0; j < results.length; j++) {
+				var element = results[j],
+					distance = element.distance.text,
+					duration = element.duration.text,
+					from = origins[i],
+					to = destinations[j];
+
+				console.log('Distance:', distance, 'Duration:', duration, 'From:', from, 'To:', to);
+			}
+		}
+	}
+}
