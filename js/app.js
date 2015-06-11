25d6acf592c09964e081a40963b4f9287a8d79ff
Ezekiel Mangalu <emangalu@gmail.com>
2015-06-11 17:05:46 +0800

    Handle form submission

--- js/app.js
+++ js/app.js
@@ -17,7 +17,8 @@ function getDistance(origin, destination, travelMode) {
 function callback(response, status) {
 	if (status == google.maps.DistanceMatrixStatus.OK) {
 		var origins = response.originAddresses,
-			destinations = response.destinationAddresses;
+			destinations = response.destinationAddresses,
+			data = {};
 
 		for (var i = 0; i < origins.length; i++) {
 			var results = response.rows[i].elements;
@@ -28,8 +29,50 @@ function callback(response, status) {
 					from = origins[i],
 					to = destinations[j];
 
-				console.log('Distance:', distance, 'Duration:', duration, 'From:', from, 'To:', to);
+				// console.log('Distance:', distance, 'Duration:', duration, 'From:', from, 'To:', to);
+				data = {
+					distance: distance,
+					duration: duration,
+					from: from,
+					to: to
+				};
 			}
 		}
+
+		return data;
 	}
 }
+
+// Handle form submission
+var queryForm 	= document.getElementById('query-form'),
+	from		= queryForm.getElementsByClassName('from')[0],
+	to		= queryForm.getElementsByClassName('to')[0],
+	resultsContainer = document.getElementsByClassName('query-results')[0];
+
+if (queryForm.attachEvent) {
+	queryForm.attachEvent('submit', submitForm);
+} else {
+	queryForm.addEventListener('submit', submitForm);
+}
+
+function submitForm(e) {
+	if (e.preventDefault) e.preventDefault();
+	
+	// Validate
+	if (from.value.trim().length > 0 && to.value.trim().length > 0)
+	{
+		var results = getDistance(from.value.trim(), to.value.trim());
+		console.log(typeof(results));
+		showResults('<p>It takes roughly', results.duration, 'to go from', results.from, 'to', results.to, '</p>');
+	} else {
+		showResults('<p class="error">You need to fill in both fields</p>');
+	}
+
+	return false;
+}
+
+// Display results
+function showResults(results) {
+	resultsContainer.innerHTML = results;
+}
+
