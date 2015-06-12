8f8af5817d6c37a03882230cea84ba98f432720c
Ezekiel Mangalu <emangalu@gmail.com>
2015-06-12 10:19:03 +0800

    Fix data not loaded, using promises

--- js/app.js
+++ js/app.js
@@ -5,74 +5,82 @@ GoogleMapsLoader.load(function (google) {
 	distanceMatrixService = new google.maps.DistanceMatrixService();
 });
 
-// Query
+// Get Distance
 function getDistance(origin, destination, travelMode) {
+	var deferred = $.Deferred();
 	distanceMatrixService.getDistanceMatrix({
 		origins: [origin],
 		destinations: [destination],
 		travelMode: travelMode || google.maps.TravelMode.DRIVING
-	}, callback);
+	}, function(response, status){
+		if (status == google.maps.DistanceMatrixStatus.OK)
+		{
+			deferred.resolve(response);
+		} else
+		{
+			deferred.reject(response);
+		}
+	});
+
+	return deferred.promise();
 }
 
-function callback(response, status) {
-	if (status == google.maps.DistanceMatrixStatus.OK) {
-		var origins = response.originAddresses,
-			destinations = response.destinationAddresses,
-			data = {};
+function parseResponse(response) {
+	var origins = response.originAddresses,
+		destinations = response.destinationAddresses,
+		data = {};
 
-		for (var i = 0; i < origins.length; i++) {
-			var results = response.rows[i].elements;
-			for (var j = 0; j < results.length; j++) {
-				var element = results[j],
-					distance = element.distance.text,
-					duration = element.duration.text,
-					from = origins[i],
-					to = destinations[j];
+	for (var i = 0; i < origins.length; i++) {
+		var results = response.rows[i].elements;
+		for (var j = 0; j < results.length; j++) {
+			var element = results[j],
+				distance = element.distance.text,
+				duration = element.duration.text,
+				from = origins[i],
+				to = destinations[j];
 
-				// console.log('Distance:', distance, 'Duration:', duration, 'From:', from, 'To:', to);
-				data = {
-					distance: distance,
-					duration: duration,
-					from: from,
-					to: to
-				};
-			}
+			// console.log('Distance:', distance, 'Duration:', duration, 'From:', from, 'To:', to);
+			data = {
+				distance: distance,
+				duration: duration,
+				from: from,
+				to: to
+			};
 		}
-
-		return data;
 	}
+
+	return data;
 }
 
 // Handle form submission
-var queryForm 	= document.getElementById('query-form'),
-	from		= queryForm.getElementsByClassName('from')[0],
-	to		= queryForm.getElementsByClassName('to')[0],
-	resultsContainer = document.getElementsByClassName('query-results')[0];
+var queryForm 	= $('#query-form'),
+	from		= queryForm.children('.from')[0],
+	to		= queryForm.children('.to')[0],
+	resultsContainer = $('.query-results');
 
-if (queryForm.attachEvent) {
-	queryForm.attachEvent('submit', submitForm);
-} else {
-	queryForm.addEventListener('submit', submitForm);
-}
-
-function submitForm(e) {
-	if (e.preventDefault) e.preventDefault();
+queryForm.submit(function (e) {
+	// console.log('submitting...');
+	e.preventDefault();
+	var originVal = from.value.trim(),
+		destinationVal = to.value.trim();
 	
 	// Validate
-	if (from.value.trim().length > 0 && to.value.trim().length > 0)
+	if (originVal.length > 0 && destinationVal.length > 0)
 	{
-		var results = getDistance(from.value.trim(), to.value.trim());
-		console.log(typeof(results));
-		showResults('<p>It takes roughly', results.duration, 'to go from', results.from, 'to', results.to, '</p>');
+		getDistance(originVal, destinationVal).then(function(data) {
+				var results = parseResponse(data);
+				showResults('<p>It takes roughly <strong>' + results.duration + '</strong> to go from <strong>' + results.from + '</strong> to <strong>' + results.to+ '</strong></p>');
+			});
 	} else {
 		showResults('<p class="error">You need to fill in both fields</p>');
 	}
 
 	return false;
-}
+});
 
 // Display results
 function showResults(results) {
-	resultsContainer.innerHTML = results;
+	resultsContainer.children().remove();
+	resultsContainer.append(results);
 }
 
